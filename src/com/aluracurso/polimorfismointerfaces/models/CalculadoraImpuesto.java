8da82bcdb8d4a81e83855ba6803126e8aee76f61
lfgarciaoc <luizafernandagarcia@gmail.com>
2024-04-18 23:41:28 -0500

    agregando interfaz Tributable y clase CalculadoraImpuesto

--- src/com/aluracurso/polimorfismointerfaces/models/CalculadoraImpuesto.java
+++ src/com/aluracurso/polimorfismointerfaces/models/CalculadoraImpuesto.java
@@ -1,2 +1,13 @@
-package com.aluracurso.polimorfismointerfaces.models;public class CalculadoraImpuesto {
+package com.aluracurso.polimorfismointerfaces.models;
+
+public class CalculadoraImpuesto {
+    private double totalImpuesto = 0;
+
+    public void calcularImpuesto(Tributable item) {
+        this.totalImpuesto += item.getValorImpuesto();
+    }
+
+    public double getTotalImpuesto() {
+        return this.totalImpuesto;
+    }
 }
--- src/com/aluracurso/polimorfismointerfaces/models/Tributable.java
+++ src/com/aluracurso/polimorfismointerfaces/models/Tributable.java
@@ -1,5 +1,9 @@
 package com.aluracurso.polimorfismointerfaces.models;
-
+/**un sistema de ventas que necesita calcular el impuesto de
+ * diferentes tipos de productos. Podemos crear la interfaz
+ * "Tributable" para definir el comportamiento de cálculo de
+ * impuestos y luego crear varias clases diferentes que implementen esta
+ * interfaz para calcular el impuesto de diferentes productos.**/
 public interface Tributable {
     double getValorImpuesto();
 }
